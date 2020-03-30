83774ebbbe14c5fa30bbb5bf0d0589226659b4dc
njcich <60636727+njcich@users.noreply.github.com>
2020-03-30 14:43:31 -0700

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -1,27 +1,27 @@
 from sklearn.cluster import KMeans
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 from utils import load_csv
 
-MAX_K = 30
+MAX_K = 10
+IDEAL_K = np.array([])
 
 def main():
     df = load_csv('world_indices.csv')
     data = df.to_numpy()
-    print(data)
-    for x in range(np.shape(data)[1]):
-        kmeans(data[x, 1:].reshape(-1, 1), df.columns[x])
+    for x in range(2, np.shape(data)[1]):
+        visualize_kmeans(data[:, x].reshape(-1, 1), df.columns[x])
 
-def kmeans(data, col_name):
+def visualize_kmeans(data, name):
     loss = np.zeros(MAX_K)
-    for K in range(MAX_K - 2):
-        kmeans = KMeans(n_clusters=(K + 2), random_state=0).fit(data)
+    loss[0] = float("inf")
+    for K in range(1, MAX_K):
+        kmeans = KMeans(n_clusters=K, random_state=0).fit(data)
         loss[K] = kmeans.inertia_
     plt.plot(loss)
     plt.xlabel('Number of Clusters')
     plt.ylabel('Loss Value')
-    plt.title(col_name)
+    plt.title(name)
     plt.show()
 
 if __name__ == '__main__':
